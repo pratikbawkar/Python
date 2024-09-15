6e206c871c1626b8a97c36679dada3eaf5a3618f
unknown <pratikbawkar33@gmail.com>
2024-09-16 00:11:43 +0530

    new learning of input output

---
+++ File_I0/code names.py
@@ -0,0 +1,11 @@
+names = []
+
+for _ in range(2):
+    names.append(input("What's your name ?"))
+
+for name in sorted(names):
+    print(f"Hello,{name}")
+
+#after running this code again all the other names are lost. if its bigger program it will pretty tough to write all names again
+# file I/O comes in where files come in where it stores information (phone,mac,cloud,pc etc )
+
